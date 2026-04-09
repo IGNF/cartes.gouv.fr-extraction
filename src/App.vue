1e317ecc51e12dbb0fc55c674876778417843446
Vincent De Oliveira <vincent@iamvdo.me>
2026-04-09 10:26:11 +0200

    feat: Utilise cartes.gouv.fr-vue-components

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,52 @@
 <script setup lang="ts">
-const serviceTitle = 'Service'
-const serviceDescription = 'Description du service'
-const logoText = ['Ministère', 'de l’intérieur']
-
-const quickLinks = [
-  {
-    label: 'Home',
-    to: '/',
-    icon: 'ri-home-4-line',
-    iconAttrs: { color: 'var(--red-marianne-425-625)' },
-  },
-  {
-    label: 'À propos',
-    to: '/a-propos',
-    icon: 'ri-question-mark',
-    iconRight: true,
-  },
-]
-const searchQuery = ref('')
+import { CgfrFooter, CgfrHeader } from 'cartes.gouv.fr-vue-components'
 </script>
 
 <template>
-  <DsfrHeader
-    v-model="searchQuery"
-    :service-title="serviceTitle"
-    :service-description="serviceDescription"
-    :logo-text="logoText"
-    :quick-links="quickLinks"
-    show-search
+  <CgfrHeader
+    class="CgfrHeader"
+    badge-text="Extraction"
+    badge-icon="fr-icon-road-map-fill"
+    badge-color="pink-macaron"
   />
 
-  <div class="fr-container  fr-mt-3w  fr-mt-md-5w  fr-mb-5w">
+  <div class="Alerts" />
+
+  <div class="fr-container  fr-mt-3w  fr-mt-md-5w  fr-mb-5w Content">
     <router-view />
   </div>
 
+  <CgfrFooter />
+
+  <Modals />
 </template>
+
+<style lang="scss">
+body {
+  min-height: 100vh;
+}
+#app {
+  display: grid;
+  // on définit 4 lignes (attention, il faut bien 4 enfants dans #app)
+  // [nom] taille
+  grid-template-rows:
+    [header] auto
+    [alerts] auto
+    [content] 1fr
+    [footer] auto;
+  min-height: 100vh;
+}
+// on place les éléments
+.CgfrHeader {
+  grid-row: header;
+}
+.Alerts {
+  grid-row: alerts;
+}
+.Content {
+  grid-row: content;
+}
+.CgfrFooter {
+  grid-row: footer;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
-import '@gouvfr/dsfr/dist/core/core.main.min.css'
-import '@gouvfr/dsfr/dist/component/component.main.min.css'
-import '@gouvfr/dsfr/dist/utility/utility.main.min.css'
-import '@gouvminint/vue-dsfr/styles'
-
-import '@gouvfr/dsfr/dist/scheme/scheme.min.css'
-import '@gouvfr/dsfr/dist/utility/icons/icons.min.css'
-
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+
+import '@gouvfr/dsfr/dist/dsfr.min.css'
+import '@gouvfr/dsfr/dist/utility/icons/icons.min.css'
+import '@gouvminint/vue-dsfr/styles'
+import 'cartes.gouv.fr-vue-components/dist/index.css'
 
 import './main.css'
 
