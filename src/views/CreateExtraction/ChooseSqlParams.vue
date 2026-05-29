7f962d5213dc810e7b0bb1cd75e3cf08b32971c5
hcaillaud <hector.caillaud@ign.fr>
2026-05-29 12:54:57 +0200

    update package

--- src/views/CreateExtraction/ChooseSqlParams.vue
+++ src/views/CreateExtraction/ChooseSqlParams.vue
@@ -10,7 +10,7 @@ const props = defineProps<{
 const model = defineModel<ExtractionRequestBody>()
 
 const code = ref()
-const format = ref('CSV')
+const format = ref('GPKG')
 const projection = ref('EPSG:4326')
 const encoding = ref('UTF-8')
 const relations = ref<RelationInput>({})
@@ -63,7 +63,7 @@ watch(requestBody, (newValue) => {
                             <div class="fr-col ">
                                 <DsfrSelect
                                     label="Format"
-                                    :options="['CSV', 'GPKG', 'SHP', 'PARQUET']"
+                                    :options="['GPKG', 'SHP', 'PARQUET']"
                                     v-model="format"
                                 />
                                 <!-- <DsfrSelect
