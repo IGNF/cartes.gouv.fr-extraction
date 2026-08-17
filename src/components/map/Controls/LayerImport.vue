190feab51e2de2893dccde68a72cebe27f0e9f45
hcaillaud <hector.caillaud@ign.fr>
2026-08-17 19:14:35 +0200

    import geojson

--- src/components/map/Controls/LayerImport.vue
+++ src/components/map/Controls/LayerImport.vue
@@ -12,6 +12,7 @@
 <script setup lang="js">
 import { useLogger } from 'vue-logger-plugin';
 import { useMapStore } from '@/stores/mapStore';
+import { useCreateExtractionStore } from '@/stores/createExtractionStore';
 
 import {
   LayerImport
@@ -34,12 +35,63 @@ const props = defineProps({
 });
 
 const mapStore = useMapStore();
+const createExtractionStore = useCreateExtractionStore();
 const log = useLogger();
 
 const map = computed(() => mapStore.getMapRef(props.mapId))
 const layerImport = ref(new LayerImport(props.layerImportOptions));
 
+// Event listeners for LayerImport
+const handleVectorAdded = (event) => {
+  const currentMap = map.value?.value;
+  const previousExtentLayer = createExtractionStore.extentLayer;
+
+  if (previousExtentLayer && currentMap) {
+    const previousLayerId = previousExtentLayer.ol_uid;
+    const mapLayers = currentMap.getLayers().getArray();
+    const layerToRemove = mapLayers.find((layer) => {
+      const layerId = layer.ol_uid;
+      return previousLayerId !== undefined && layerId === previousLayerId;
+    });
+
+    if (layerToRemove) {
+      currentMap.getLayers().remove(layerToRemove);
+      createExtractionStore.removeExtentLayer();
+    }
+  }
+
+  createExtractionStore.setExtentLayer(event.layer);
+};
+
+const handleMapboxAdded = (event) => {
+  log.debug('MapBox layer added:', event);
+};
+
+const handleServiceAdded = (event) => {
+  log.debug('Service layer added:', event);
+};
+
+const handleEditorLoaded = (event) => {
+  log.debug('Editor loaded:', event);
+};
+
+const handleRenderSuccess = (event) => {
+  log.debug('Render success:', event);
+};
+
+const handleRenderFailure = (event) => {
+  log.debug('Render failure:', event);
+};
+
 onMounted(() => {
+  // Add event listeners
+  layerImport.value.addEventListener('layerimport:vector:added', handleVectorAdded);
+  layerImport.value.addEventListener('layerimport:mapbox:added', handleMapboxAdded);
+  layerImport.value.addEventListener('layerimport:service:added', handleServiceAdded);
+  layerImport.value.addEventListener('editor:loaded', handleEditorLoaded);
+  layerImport.value.addEventListener('render:success', handleRenderSuccess);
+  layerImport.value.addEventListener('render:failure', handleRenderFailure);
+
   watch(
     () => map.value,
     (currentMap) => {
@@ -49,6 +101,17 @@ onMounted(() => {
     },
     { immediate: true }
   )
+})
+
+onUnmounted(() => {
+  // Remove event listeners on component unmount
+  const element = layerImport.value;
+  element.removeEventListener('layerimport:vector:added', handleVectorAdded);
+  element.removeEventListener('layerimport:mapbox:added', handleMapboxAdded);
+  element.removeEventListener('layerimport:service:added', handleServiceAdded);
+  element.removeEventListener('editor:loaded', handleEditorLoaded);
+  element.removeEventListener('render:success', handleRenderSuccess);
+  element.removeEventListener('render:failure', handleRenderFailure);
 })
 </script>
 
@@ -61,5 +124,21 @@ onMounted(() => {
 // mais le bouton est caché (car intégré dans menus gauche et droite)
 .position-container-top-left .gpf-btn-icon[id^=GPshowImportPicto-] {
   display: none;
+}
+div[id^='GPimportStaticParams-'] > .GPimportInputLine:first-child {
+  display: none;
+}
+div[id^='GPimportStaticParams-'] {
+  margin-bottom: 0;
+}
+dialog[id^='GPimportPanel-'] {
+  max-height: calc(45vh - 20px) !important;
+}
+form[id^='GPimportForm-'] {
+  margin-bottom: 0;
+  overflow: hidden;
+}
+input[id^='GPimportSubmit-'] {
+  margin-top: 0
 }
 </style>
--- src/stores/createExtractionStore.ts
+++ src/stores/createExtractionStore.ts
@@ -1,15 +1,42 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { ExtractionRequestBody } from '@/types/extractibles.types'
+import type VectorLayer from 'ol/layer/Vector'
 
 export const useCreateExtractionStore = defineStore('createExtraction', () => {
   const selectedExtractibleID = ref<string | null>(null)
   const requestBody = ref<ExtractionRequestBody | undefined>(undefined)
+  const extentLayer = shallowRef<VectorLayer | null>(null)
+  const extent = computed(() => {
+    if (!extentLayer.value) return null
+    const source = extentLayer.value.getSource()
+    if (!source) return null
+    const sourceExtent = source.getExtent?.()
+    if (!sourceExtent) return null
+    return sourceExtent
+  })
+
+  function setExtentLayer(layer: VectorLayer) {
+    extentLayer.value = layer
+  }
+
+  function removeExtentLayer() {
+    extentLayer.value = null
+  }
 
   function reset() {
     selectedExtractibleID.value = null
     requestBody.value = undefined
+    extentLayer.value = null
   }
 
-  return { selectedExtractibleID, requestBody, reset }
+  return { 
+    selectedExtractibleID,
+    requestBody,
+    extentLayer,
+    extent,
+    setExtentLayer,
+    removeExtentLayer,
+    reset
+  }
 })
